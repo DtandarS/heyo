9fbce5124819ff7de12d4f9543388d3b6f12777e
DtandarS <97455448+DtandarS@users.noreply.github.com>
2025-05-24 00:12:22 +0300

    some changes into hello.c

--- hello.c
+++ hello.c
@@ -10,25 +10,73 @@ const int screenWidth = 800;
 const int screenHeight = 600;
 
 
+// Testing Error function
+
+
+// Error types all in here
+
+void ErrorCode1(){
+
+  // This error appears whenever SDL could not initialize
+  printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+
+}
+
+void ErrorCode2(){
+
+  // This error appears whenver SDL can not create a window
+  printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+
+}
+
+
+// This section houses all the components that are used for code
+
+void SDLInitCheck(){
+
+  SDL_Surface* screenSurface = NULL;
+
+
+  //This will initialize SDL
+
+  if (SDL_Init(SDL_INIT_VIDEO) == -1){
+
+    ErrorCode1();
+    return;      // <-- took me almost 30min to figure out how to end function without it proceeding outside if statement. This ensures that if the error occurs. The program won't try to proceed and writes the error on to the terminal
+
+  } 
+
+  printf(" The SDL was initialized succesfully");    // <-- might be unnecessary but this helps to confirm SDL initialization to the terminal.
+
+}
+
+void windowCreation (){
+
+  SDL_Window* screenWindow = NULL;
+
+
+  // Create window
+  screenWindow = SDL_CreateWindow( "SDL Tutorial", screenWidth, screenHeight, SDL_WINDOW_BORDERLESS);
+
+  if( screenWindow == NULL ) {
+
+    ErrorCode2();
+    return;
+
+  }
+
+  printf("Window Created Succesfully");
+
+}
+
+
 // main() function
 
 int main(int argC, char* argS[]){
 
-  SDL_Window* screenWindow = NULL;
+  SDLInitCheck();
 
-  SDL_Surface* screenSurface = NULL;
+  windowCreation();
 
-  if (SDL_Init(SDL_INIT_VIDEO) < 0){
-
-    ErrorCode1()
-
-  } 
-
-}
-
-int ErrorCode1(){
-
-  printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-  
 }
 
